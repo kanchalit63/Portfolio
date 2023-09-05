c2539d086a7bd14833616736ff8ec6690b5fd2ff
kanchalit63 <kanchalit.ba63@gmail.com>
2023-09-05 15:48:25 +0700

    edit details

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -15,13 +15,15 @@ function Portfolio() {
             <img src={pro1} alt="" />
           </div>
           <div className="content">
-              <span className="title">
-                โปรแแกรมคำนวณรายรับ-รายจ่าย
-              </span>
+            <span className="title">
+              โปรแแกรมคำนวณรายรับ-รายจ่าย
+            </span>
             <p className="desc">
               เป็นโปรเเกรมแรกที่ได้เรียนหรือศึกษาจากใน Youtube ส่วนของ React และได้ทำ mini project คือ โปรแแกรมคำนวณรายรับ-รายจ่าย Credit.KongRuksiam
             </p>
-            
+            <a className="action-github" href="https://github.com/kanchalit63/Learning-First-React" target='blank'>
+              GitHub
+            </a>
             <a className="action" href="https://kanchalit63.github.io/Learning-First-React/" target='blank'>
               Demo
               <span aria-hidden="true">
@@ -32,15 +34,19 @@ function Portfolio() {
         </div>
         <div className="card-port">
           <div className="image">
-          <img src={pro2} alt="" />
+            <img src={pro2} alt="" />
           </div>
           <div className="content">
-              <span className="title">
-                React-Unsplash-API
-              </span>
+            <span className="title">
+              React-Unsplash-API
+            </span>
             <p className="desc">
               เป็นการศึกษาการใช้งานระหว่างตัวของหน้าบ้านที่ร้องขอ API จาก Unsplash มาใช้งานเพื่อแสดงผลลัพธ์ที่ได้ออกมาเมื่อกรอก Keyword เข้า Input ไป
             </p>
+
+            <a className="action-github" href="https://github.com/kanchalit63/React-unsplash-api" target='blank'>
+              GitHub
+            </a>
 
             <a className="action" href="https://kanchalit63.github.io/React-unsplash-api/" target='blank'>
               Demo
@@ -48,21 +54,24 @@ function Portfolio() {
                 →
               </span>
             </a>
+
           </div>
         </div>
         <div className="card-port">
           <div className="image">
-          <img src={pro3} alt="" />
+            <img src={pro3} alt="" />
           </div>
           <div className="content">
-              <span className="title">
-                React-Todo-List
-              </span>
+            <span className="title">
+              React-Todo-List
+            </span>
 
             <p className="desc">
               เป็นการศึกษาการสร้าง โดยใช้ React Hook มาใช้งานเพื่อเก็บค่าและนำมาแสดงผล รวมถึง Action ในการทำงานอีกด้วย โดยสามารถทำงานได้ด้วยคือ การสร้าง ลบ แก้ไข และAction Complete
             </p>
-
+            <a className="action-github" href="https://github.com/kanchalit63/React-Todo-List" target='blank'>
+              GitHub
+            </a>
             <a className="action" href="https://kanchalit63.github.io/React-Todo-List/" target='blank'>
               Demo
               <span aria-hidden="true">
@@ -73,17 +82,19 @@ function Portfolio() {
         </div>
         <div className="card-port">
           <div className="image">
-          <img src={pro4} alt="" />
+            <img src={pro4} alt="" />
           </div>
           <div className="content">
-              <span className="title">
-                React-Weather-Api
-              </span>
+            <span className="title">
+              React-Weather-Api
+            </span>
 
             <p className="desc">
-            เป็นการศึกษาการใช้งานระหว่างตัวของหน้าบ้านที่ร้องขอ API จาก Weather-Api โดยเมื่อมีการกรอกชื่อเมืองหรือจังหวัดเข้าในตัวของ Input จะทำการแสดงผล รายละเอียดของสภาพอากาศออกมา
+              เป็นการศึกษาการใช้งานระหว่างตัวของหน้าบ้านที่ร้องขอ API จาก Weather-Api โดยเมื่อมีการกรอกชื่อเมืองหรือจังหวัดเข้าในตัวของ Input จะทำการแสดงผล รายละเอียดของสภาพอากาศออกมา
             </p>
-
+            <a className="action-github" href="https://github.com/kanchalit63/React-Weather-App" target='blank'>
+              GitHub
+            </a>
             <a className="action" href="https://kanchalit63.github.io/React-Weather-App/" target='blank'>
               Demo
               <span aria-hidden="true">
